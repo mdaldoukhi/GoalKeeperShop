491caffd9dd4d448f8c2d5f7c417cad6a62c5143
Meshari Aldoukhi <mashry151@hotmail.com>
2021-06-07 19:54:21 +0300

    Created Gloves array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import shopImage from './GoalKeeper.png';
+import gloves from './gloves'
 function App() {
+
+  //Gitting the data from gloves.js
+  const glovesList = gloves.map((gloves) => (
+    <div key={gloves.id}>
+      <img className="image-product" src={gloves.image} />
+      <p className="product-name">{gloves.name}</p>
+      <p className="product-price">Price: <span>{gloves.price}</span> KD</p>
+    </div>
+  ));
+  
   return (
     <div>
-      <h1 className="text-center text-line">GoalKeeper Station</h1>
-      <h4 className="descrption">Welcome to the official goalkeeper online shop. We know you love soccer, you are a goalkeeper at heart, you excercise hard, you are an important part of your team.</h4>
-      <img id ="shopImage" src={shopImage} />
+      <div>
+        <h1 className="text-center text-line">GoalKeeper Station</h1>
+        <h4 className="descrption">Welcome to the official goalkeeper online shop. We know you love soccer, you are a goalkeeper at heart, you excercise hard, you are an important part of your team.</h4>
+        <img id="shopImage" src={shopImage} />
+      </div>
+      <div>
+        <h1 className="title">Gloves Product</h1>
+        <div className="List">
+          {/* Call the data from Gloves  */}
+          {glovesList}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/gloves.js
@@ -0,0 +1,39 @@
+const gloves = [
+    {
+        id: 1,
+        name: "adidas Predator Pro Hybrid",
+        price: 10,
+        image:
+            "https://www.soccerpro.com/wp-content/uploads/fs0410_adidas_predator_pro_hybrid_gk_gloves_inflight_01.jpg",
+    },
+    {
+        id: 2,
+        name: "Nike Vapor Grip3 Goalkeeper Gloves",
+        price: 20,
+        image:
+            "https://www.soccerpro.com/wp-content/uploads/cn5650_010_nike_vapor_grip3_gk_gloves_black_laser_orange_with_white_01.jpg",
+    },
+    {
+        id: 3,
+        name: "Nike Phantom Shadow Goalkeeper Gloves",
+        price: 10,
+        image:
+            "https://www.soccerpro.com/wp-content/uploads/cn6758_406_nike_phantom_shadow_gk_gloves_photo_blue_black_with_silver_01.jpg",
+    },
+    {
+        id: 4,
+        name: "Nike Phantom Elite Goalkeeper Gloves",
+        price: 30,
+        image:
+            "https://www.soccerpro.com/wp-content/uploads/cn6724_406_nike_phantom_elite_gk_gloves_photo_blue_black_with_silver_01.jpg",
+    },
+    {
+        id: 5,
+        name: "Nike Match Goalkeeper Gloves",
+        price: 100,
+        image:
+            "https://www.soccerpro.com/wp-content/uploads/cq7799_635_nike_match_gk_gloves_bright_crimson_platinum_tint_with_indigo_burst_01.jpg",
+    },
+]
+
+export default gloves;
